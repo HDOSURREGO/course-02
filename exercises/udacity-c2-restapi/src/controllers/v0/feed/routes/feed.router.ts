4bc6fbe8260b90caf2f4fa258f30be65365b2c90
Henry Hoyos <hdosurrego@gmail.com>
2020-04-13 23:07:38 -0400

    bcyrpt error solved by intstalling bcryptjs with @Types

--- exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -19,7 +19,8 @@ router.get("/", async (req: Request, res: Response) => {
 //@TODO
 //Add an endpoint to GET a specific resource by Primary Key
 router.get("/:id", async (req: Request, res: Response) => {
-	const item = await FeedItem.findById;
+	let id = req.params.id;
+	const item = await FeedItem.findByPk(id);
 	res.status(200).send(item);
 });
 
